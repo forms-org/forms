37237c4353d518c0a0d35fcf46080644a6182207
Connor Lien <connorlien19@gmail.com>
2022-09-17 12:46:44 -0700

    FEAT: isodd and sin math functions

--- forms/executor/dfexecutor/utils.py
+++ forms/executor/dfexecutor/utils.py
@@ -12,7 +12,38 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import pandas as pd
+import numpy as np
+
+from forms.executor.table import DFTable
 from forms.executor.executionnode import FunctionExecutionNode
+
+
+def construct_df_table(array):
+    return DFTable(df=pd.DataFrame(array))
+
+
+def get_value_rr(df: pd.DataFrame, window_size: int, func1, func2) -> pd.DataFrame:
+    return df.agg(func1, axis=1).rolling(window_size, min_periods=window_size).agg(func2).dropna()
+
+
+def get_value_ff(single_value, n_formula: int) -> pd.DataFrame:
+    return pd.DataFrame(np.full(n_formula, single_value))
+
+
+def get_value_fr(df: pd.DataFrame, min_window_size: int, func1, func2) -> pd.DataFrame:
+    return df.agg(func1, axis=1).expanding(min_window_size).agg(func2).dropna()
+
+
+def get_value_rf(df: pd.DataFrame, min_window_size: int, func1, func2) -> pd.DataFrame:
+    return df.iloc[::-1].agg(func1, axis=1).expanding(min_window_size).agg(func2).dropna().iloc[::-1]
+
+
+def fill_in_nan(value, n_formula: int) -> pd.DataFrame:
+    if n_formula > len(value):
+        extra = pd.DataFrame(np.full(n_formula - len(value), np.nan))
+        value = pd.concat([value, extra], ignore_index=True)
+    return value
 
 
 # TODO
