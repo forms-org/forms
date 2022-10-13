c2a8a63ac97afb821c0f34f628cf85a84e290e41
Zixuan Yi <yizx-1017@github.com>
2022-10-13 10:30:40 +0800

    add more scripts

---
+++ benchmark/test_different_directions.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+declare -a RUN_OPTIONS=(1 2 3)
+declare FILENAME='weather.csv'
+declare FORMULA=formula_for_different_directions.csv
+declare ROWS=1000000
+declare CORES=32
+
+n=0
+sed 1d $FORMULA | while IFS="," read -r FORMULA_STR
+do
+  echo $FORMULA_STR
+  n=$(($n+1))
+	for RUN in "${RUN_OPTIONS[@]}"
+  do
+    FILE_DIR="results/TEST-DIRECTION/${n}/ROW-FIRST/RUN${RUN}"
+    mkdir -p $FILE_DIR
+    rm -f $FILE_DIR/*
+    timeout 5m python3 test_driver.py \
+              --filename "$FILENAME" \
+              --formula_str "$FORMULA_STR" \
+              --cores "$CORES" \
+              --along_row_first True \
+              --row_num "$ROWS" \
+              --output_path "$FILE_DIR" &> $FILE_DIR/run.log
+    echo "finished $FILE_DIR"
+
+    FILE_DIR="results/TEST-DIRECTION/${n}/COL-FIRST/RUN${RUN}"
+    mkdir -p $FILE_DIR
+    rm -f $FILE_DIR/*
+    timeout 5m python3 test_driver.py \
+              --filename "$FILENAME" \
+              --formula_str "$FORMULA_STR" \
+              --cores "$CORES" \
+              --along_row_first False \
+              --row_num "$ROWS" \
+              --output_path "$FILE_DIR" &> $FILE_DIR/run.log
+    echo "finished $FILE_DIR"
+  done
+done
+
--- forms/executor/costmodel.py
+++ forms/executor/costmodel.py
@@ -13,6 +13,7 @@
 #  limitations under the License.
 from abc import ABC, abstractmethod
 from enum import Enum, auto
+from time import time
 
 from pynverse import inversefunc
 
@@ -30,6 +31,7 @@ from forms.utils.reference import RefType
 class BaseCostModel(ABC):
     def __init__(self, num_of_formulae: int):
         self.num_of_formulae = num_of_formulae
+        self.time_cost = 0
 
     @abstractmethod
     def cost(self, subtree: ExecutionNode, cores: int):
@@ -54,9 +56,12 @@ class SimpleCostModel(BaseCostModel, ABC):
         return self.num_of_formulae
 
     def get_partition_plan(self, subtree: ExecutionNode, cores: int):
+        start_time = time()
         num_of_formulae = self.num_of_formulae
         partitions = [int(i * num_of_formulae / cores) for i in range(cores)]
         partitions.append(num_of_formulae)
+        end_time = time()
+        self.time_cost += end_time - start_time
         return partitions
 
 
@@ -108,6 +113,7 @@ class LoadBalanceCostModel(BaseCostModel, ABC):
         return self.f_compute(subtree, self.num_of_formulae)
 
     def get_partition_plan(self, subtree: ExecutionNode, cores: int):
+        start_time = time()
         Q = inversefunc(lambda p: self.F(subtree, p, self.num_of_formulae))
         partitions = [0]
         for i in range(1, cores):
@@ -115,6 +121,8 @@ class LoadBalanceCostModel(BaseCostModel, ABC):
             res = Q(p)
             partitions.append(int(res))
         partitions.append(self.num_of_formulae)
+        end_time = time()
+        self.time_cost += end_time - start_time
         return partitions
 
 
